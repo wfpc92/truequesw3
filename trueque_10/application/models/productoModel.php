dddd8c1c1e112bcd585c7d1c16042c63c66b2ac0
wfpantoja <wfpantoja@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-05-25 17:53:07 +0000

    Se hizo la consulta para que generara productos segun varias palabras claves.. por ejemplo si teclean truwque tablet les aparecera dos resultados que contienen esas palabras NOTA: no estan mal escritas, se escriben asi a proposito para que busque especificamente eso.

--- trueque_10/application/models/productoModel.php
+++ trueque_10/application/models/productoModel.php
@@ -10,7 +10,7 @@ class ProductoModel extends CI_Model {
     }
 
     function getProductos() {
-         $this->db->select('producto_id, producto.nombre AS p_nombre, descripcion, categoria, imagen, fechaingreso, usuarios.usuario_id AS u_id, usuarios.nombre AS u_nombre, usuarios.apellido AS u_apellido');
+        $this->db->select('producto_id, producto.nombre AS p_nombre, descripcion, categoria, imagen, fechaingreso, usuarios.usuario_id AS u_id, usuarios.nombre AS u_nombre, usuarios.apellido AS u_apellido');
         $this->db->from('producto');
         $this->db->join('usuarios', 'producto.usuario_id = usuarios.usuario_id');
         $data = $this->db->get();
@@ -35,26 +35,31 @@ class ProductoModel extends CI_Model {
         $consulta->free_result();
         return $data;
     }
-    
-    public function buscarProductos($criterio){
+
+    public function buscarProductos($criterio) {
         /* creamos una variable array vacia */
         $data = array();
+        $this->load->helper('string');
         /* se hace la consulta sobre la base de datos */
+        $valores = explode(" ", $criterio);
         $this->db->select('producto_id, producto.nombre AS p_nombre, descripcion, categoria, imagen, fechaingreso, usuarios.usuario_id AS u_id, usuarios.nombre AS u_nombre, usuarios.apellido AS u_apellido');
         $this->db->from('producto');
         $this->db->join('usuarios', 'producto.usuario_id = usuarios.usuario_id');
-        $this->db->like('producto.nombre',$criterio);
-        $this->db->or_like('descripcion', $criterio);
-        $this->db->or_like('categoria', $criterio);
+        $this->db->like('producto.nombre', $criterio);
+        foreach ($valores as $valor):
+            $this->db->or_like('producto.nombre', $valor);
+            $this->db->or_like('descripcion', $valor);
+            $this->db->or_like('categoria', $valor);
+        endforeach;
         /* nos aseguramos de que pare de buscar cuando encuentre el primer resultado puesto
           a que estamos buscando la tupla   con su llave asi que solo habra un resultado */
         /* obtenemos y guardamos el resultado de la consulta
           dentro de la variable consulta */
-        
+
         $consulta = $this->db->get();
         foreach ($consulta->result() as $prod):
-           echo '';
-       endforeach;
+            echo '';
+        endforeach;
         /* se comprueba si hubo algun resultado en la consulta,si es asi se asigna a 
           data este resultado, de lo contrario data se retorna como se declaro, vacio */
         if ($consulta->num_rows() > 0) {
@@ -64,44 +69,43 @@ class ProductoModel extends CI_Model {
         $consulta->free_result();
         return $data;
     }
-    function cargarCategoria()
-    {
+
+    function cargarCategoria() {
         $sql = "SELECT DISTINCT categoria FROM producto";
         $query = $this->db->query($sql);
-        $data = array(); 
+        $data = array();
         if ($query->num_rows() > 0) {
             $data[""] = "Todas las categorias";
             foreach ($query->result_array() as $row) {
-                $data[$row['categoria']] = strtoupper($row['categoria']); 
+                $data[$row['categoria']] = strtoupper($row['categoria']);
             }
             return $data;
         }
         return $data;
         $query->free_result();
     }
-    
-    
-    function cargarCiudad()
-    {
-        
+
+    function cargarCiudad() {
+
         $sql = "SELECT nombre FROM cuidad";
         $query = $this->db->query($sql);
-        $ciudades = array(); 
+        $ciudades = array();
         if ($query->num_rows() > 0) {
             $ciudades[""] = "Todos";
             foreach ($query->result_array() as $row) {
-                $ciudades[$row['nombre']] = strtoupper($row['nombre']); 
+                $ciudades[$row['nombre']] = strtoupper($row['nombre']);
             }
             return $ciudades;
         }
         return $ciudades;
         $query->free_result();
     }
-    function getMisProductos($id){
+
+    function getMisProductos($id) {
         $this->db->select('producto_id, producto.nombre AS p_nombre, descripcion, categoria, imagen, fechaingreso, usuarios.usuario_id AS u_id, usuarios.nombre AS u_nombre, usuarios.apellido AS u_apellido');
         $this->db->from('producto');
         $this->db->join('usuarios', 'producto.usuario_id = usuarios.usuario_id');
-        $this->db->where('producto.usuario_id',$id);
+        $this->db->where('producto.usuario_id', $id);
         $data = $this->db->get();
         if ($data->num_rows() > 0) {
             return $data;
@@ -109,4 +113,5 @@ class ProductoModel extends CI_Model {
             return FALSE;
         }
     }
+
 }
