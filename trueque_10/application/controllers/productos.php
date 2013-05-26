e603351aa98a02f338254970c07b1d6aa5ba1e54
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-05-26 18:36:11 +0000

    se ha agregado la función busqueda avanzada, en el controlador

--- trueque_10/application/controllers/productos.php
+++ trueque_10/application/controllers/productos.php
@@ -110,6 +110,46 @@ class Productos extends CI_Controller {
          $data['contenido'] = 'busquedaAvanzada';
          $data['categoria'] = $this->productoModel->cargarCategoria();
          $data['ciudades'] = $this->productoModel->cargarCiudad();
+         
          $this->load->view('plantilla', $data);
     }
+    
+    
+     public function busquedaAvanzadaProducto(){
+        
+         $data['title']='Trueque Buscar Producto';
+		$data['sidebar']='sidebarCategorias';
+		$data['contenido'] = 'contenido';
+		$usuarioActual=$this->session->all_userdata();
+        if(isset($usuarioActual['nombre']))
+        	{
+        		$data['sesion']='sesionUsuario';
+        		$data['menu']='menuUsuario';
+        		$data['usuarioActual'] = $usuarioActual;
+                if($usuarioActual['usuario_nivel']==0){$data['menu']='menuAdministrador';}
+        	}	
+        	else
+        	{
+        		$data['sesion']='sesionLogin';
+        		$data['menu']='menuEstandar';
+        	}
+        
+        
+         $categoria= $_POST['categorias'];
+         $desde= $_POST['fechaIngreso'];
+       
+         $hasta= $_POST['hasta'];
+         $ciudad=  $_POST['ciudades'];
+                                           
+         //echo 'en el controlador'.$categoria;
+         //echo $categoria;
+        //$busquedaAvanzada['data']=$this->productoModel->busquedaAvanzadaProducto($categoria,$desde,$hasta, $ciudad);
+        //$this->load->view('Borrador',$busquedaAvanzada);
+        
+        $data['contenido'] = 'contenido';
+        $data['productos'] =$this->productoModel->busquedaAvanzada($categoria,$desde,$hasta, $ciudad);
+        $this->load->view('plantilla', $data);
+    }
+    
+    
 }
