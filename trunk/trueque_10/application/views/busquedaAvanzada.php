4e3e8c0d4ac7668718c07eb237a7b26a7647d69b
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-02 00:03:45 +0000

    modifique una linea, con el fin de mejorar la apariencia de la busqueda avanzada

--- trunk/trueque_10/application/views/busquedaAvanzada.php
+++ trunk/trueque_10/application/views/busquedaAvanzada.php
@@ -1,56 +1,65 @@
-      <div id="titulo" >
-           </br></br></br>
-           <h2 >Busqueda Avanzada </h2>
-           </br></br></br>
-       </div>
+      
+  
+
+     <table width="430"  align="left">
+         <tr>
+             <td width="200" align="center">
+                  <br/><br/>
+                  <h2 >Busqueda Avanzada </h2>
+                  <br/><br/>
+              </td>
+         </tr>
+     </table>
        
        <div id="formulario">
             <?php 
                 echo form_open('productos/busquedaAvanzadaProducto');
-                echo form_label("Categoria " );
-                echo form_dropdown('categorias', $categoria);  
+                
             ?>
        </div>
-       <br/>
-       <div >
-            </br></br>
-            <font align="center">fecha de publicacion  </font>
-            <br/><br/>
-       </div>
-       <div >
-                <td>desde: </td>
-                <td><input type="text" id="fechaIngreso" size="20" name= "fechaIngreso" readonly/></td> 
-                </br></br>
-          
-       </div>
-        <div >
-                <td>hasta : </td>
-                <td><input type="text" id="hasta" size="20" name="hasta" readonly/></td>
-       </div>
-       <br/><br/>
-       <font> ubicacion </font>
-       <br/><br/>
-         <div>
-           <?php
-                echo form_label("Ciudad");
-                echo form_dropdown('ciudades', $ciudades);     
-           ?>
-       </div>
-       <div>
-           <br/>
-           <div align="center">
-               
-               <?php
+
+<table id="registrar" width="500"  align="left">
+    <tr >
+		<td width="200" align="right">  Categoria: </td>
+		<td> <?php echo form_dropdown('categorias', $categoria);?></td>
+   </tr>
+ 
+   <tr>
+       <td width="200" align="left"><font align="center">Fecha de publicaci&oacute;n  </font></td>
+   </tr>
+   <tr>
+       <td width="200" align="right">Desde: </td>
+       <td><input type="text" id="fechaIngreso" size="15" name= "fechaIngreso" readonly/></td> 
+   </tr>   
+   <tr>
+       <td width="200" align="right">Hasta : </td>
+       <td><input type="text" id="hasta" size="15" name="hasta" readonly/></td>
+   </tr>
+   <tr>
+       <td width="200" align="left"><font> Ubicaci&oacute;n </font></td>
+   </tr>   
+   <tr>
+       <td width="200" align="right">Ciudad: </td>
+       <td><?php echo form_dropdown('ciudades', $ciudades);    ?></td>
+   </tr> 
+   
+    
+</table>
+
+<table align="center" width="180">
+    <tr>
+        <td align="right">
+         <br/>
+         <?php
                     echo form_open('productos/index');
                     echo form_button('btnCancelar','Cancelar');
                     echo form_submit('btnBuscar', 'Buscar' ); 
                     echo form_close();
                     echo form_close();
                 ?>
-           </div>
-       </div>
-        
-        
-
+            </td>
+    </tr>
+</table>
+       
 
         <div style="clear: both;">&nbsp;</div>
