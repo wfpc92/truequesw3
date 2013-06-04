5968df307287d81c6cf05c7217c727b9d862a33c
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-04 19:50:09 +0000

    en el modelo permutaModel se hicieron las consultas para ver las permutas y para hacer una permuta, es decir, le cambia la fecha de 0000-00-00 a el CURRENT_TIMESTAMP que es la fecha actual, es decir que en la tabla de la base de datos permuta, quienes tengan 0000-00-00 es porque no han hecho trueque, las que si tengan fecha asignada si lo hicieron, posteriormente se cambia de dueño los productos.

--- trunk/trueque_10/application/models/permutaModel.php
+++ trunk/trueque_10/application/models/permutaModel.php
@@ -9,14 +9,21 @@ class PermutaModel extends CI_Model {
         parent::__construct();
     }
 
-    function getPermutas() {
-        $this->db->select('producto_recibe, producto_solicita, fechapermuta, nombre, descripcion, categoria.nombre, imagen,fechaingreso');
-        $this->db->from('permuta');
-        $this->db->join('producto', 'producto.producto_id = producto_recibe');
-        $this->db->join('producto', 'producto.producto_id = producto_solicita');
-        $this->db->join('usuarios', 'producto.usuario_id = usuarios.usuario_id');
-        $this->db->join('categoria', 'categoria.categoria_id = producto.categoria_id');
-       
+    function getPermutas($current) {
+        
+        
+        $this->db->select('usu_solicita.usuario_id AS usu_solicita_id, usu_recibe.usuario_id AS usu_recibe_id, p1.nombre AS rec_nombre ,p2.nombre AS sol_nombre, p1.imagen AS rec_imagen,p2.imagen AS sol_imagen, usu_solicita.nombre AS sol_usu_nombre, usu_solicita.apellido AS sol_usu_apellido, per.producto_recibe AS rec_producto_id, per.producto_solicita AS sol_producto_id, usu_solicita.usuario_id AS sol_usu_id, per.producto_recibe AS rec_producto_id, per.producto_solicita AS sol_producto_id');
+        $this->db->from('permuta AS per');
+        $this->db->join('producto AS p1', 'p1.producto_id = per.producto_recibe');
+        $this->db->join('producto AS p2', 'p2.producto_id = per.producto_solicita');
+        $this->db->join('usuarios AS usu_recibe', 'p1.usuario_id = usu_recibe.usuario_id');
+        $this->db->join('usuarios AS usu_solicita', 'p2.usuario_id = usu_solicita.usuario_id');
+        $this->db->where('usu_recibe.usuario_id',  $current);
+        $this->db->where('per.fechapermuta',  '0000-00-00');
+        
+        
+        
+
         $data = $this->db->get();
         if ($data->num_rows() > 0) {
             return $data;
@@ -24,6 +31,23 @@ class PermutaModel extends CI_Model {
             return FALSE;
         }
     }
+    
+    function hacerPermuta($peticion){
+        echo $peticion;
+        list($recibe, $solicita, $usu_recibe, $usu_solicita) = explode(".", $peticion);  
+        
+        $query = $this->db->query('UPDATE permuta SET fechapermuta = CURRENT_TIMESTAMP WHERE producto_recibe = '.$recibe.' AND producto_solicita = '.$solicita); 
+        $query1=  $this->db->query('update producto set usuario_id='.$usu_solicita.' where producto_id='.$recibe);
+        $query2=  $this->db->query('update producto set usuario_id='.$usu_recibe.' where producto_id='.$solicita);
+    }
+    
+    
+    function rechazarPermuta($peticion){
+        echo $peticion;
+        list($recibe, $solicita, $usu_recibe, $usu_solicita) = explode(".", $peticion);  
+        $query = $this->db->query('DELETE FROM permuta where producto_recibe = '.$recibe.' and producto_solicita = '.$solicita.' and fechapermuta = \'0000-00-000\'');
+    }
+    
 /*
     function getProducto($id) {
         $data = array();
