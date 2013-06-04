b7cf58cd45ded1cea928de6fa91c9dc63f54442e
jennifererazo0124 <jennifererazo0124@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-04 00:32:35 +0000

    Se han agregado las funciones necesarias para obtener los productos de cada categoria del sidebarCategorias con su respectiva paginacion

--- trunk/trueque_10/application/controllers/productos.php
+++ trunk/trueque_10/application/controllers/productos.php
@@ -182,5 +182,112 @@ class Productos extends CI_Controller {
         $data['productos'] = $this->productoModel->busquedaAvanzada($categoria, $desde, $hasta, $ciudad);
         $this->load->view('plantilla', $data);
     }
+    
+    /*sidebar*/
+    
+    function getAntiguedades(){
+        $this->getProductosSide(1);
+    }
+
+    function getCamaras(){
+        $this->getProductosSide(2);
+    }
+    
+    function getCasas(){
+        $this->getProductosSide(3);
+    }
+    
+    function getCelulares(){
+        $this->getProductosSide(4);
+    }
+    
+    function getCine(){
+        $this->getProductosSide(5);
+    }
+    
+    function getComputadores(){
+        $this->getProductosSide(6);
+    }
+    
+    function getDeporte(){
+        $this->getProductosSide(7);
+    }
+    
+    function getElectrodomesticos(){
+        $this->getProductosSide(8);
+    }
+    
+    function getJoyas(){
+        $this->getProductosSide(9);
+    }
+    
+    function getJuguetes(){
+        $this->getProductosSide(10);
+    }
+    
+    function getLibros(){
+        $this->getProductosSide(11);
+    }
+    
+    function getLicores(){
+        $this->getProductosSide(12);
+    }
+    
+    function getMusica(){
+        $this->getProductosSide(13);
+    }
+    
+    function getVehiculos(){
+        $this->getProductosSide(14);
+    }
+    
+    function getVideojuegos(){
+        $this->getProductosSide(15);
+    }
+    
+    function getOtros(){
+        $this->getProductosSide(16);
+    }
+    
+    public function getProductosSide($categoria){
+
+         $data['contenido'] = 'estandar/inicio';
+         $data['title'] = 'Trueque Inicio';
+         $data['sidebar'] = 'sidebarCategorias';
+         $usuarioActual = $this->session->all_userdata();
+         if (isset($usuarioActual['nombre'])) {
+            $data['sesion'] = 'sesionUsuario';
+            $data['menu'] = 'menuUsuario';
+            $data['usuarioActual'] = $usuarioActual;
+            if ($usuarioActual['usuario_nivel'] == 0) {
+                $data['menu'] = 'menuAdministrador';
+            }
+        } else {
+            $data['sesion'] = 'sesionLogin';
+            $data['menu'] = 'menuEstandar';
+        }
+        //PAGINACION...
+        $this->load->library('pagination');
+
+        $opciones = array();
+        $opciones['per_page'] = 5;
+        $opciones['base_url'] = base_url() . 'productos/index';
+        $opciones['total_rows'] = $this->productoModel->getNumProductosCategoria($categoria);
+        $opciones['uri_segment'] = 3;
+        $opciones['num_links'] = 3;
+        $opciones['first_link'] = 'Primero';
+        $opciones['last_link'] = 'Ultimo';
+
+
+        $this->pagination->initialize($opciones);
+        $productos = $this->productoModel->getCamaras($opciones['per_page'], $this->uri->segment(3),$categoria);
+        $data['productos'] = $productos;
+        //FIN_PAGINACION...
+
+        $this->load->view('plantilla', $data);
+
+  
+    }
+    
 
 }
