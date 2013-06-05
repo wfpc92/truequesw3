7d05e9e11b8f45000f20fd8476720343b122632b
jessadrada <jessadrada@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-05 00:46:14 +0000

    se coloco dentro de un div el nombre del usuario

--- trunk/trueque_10/application/views/includes/sesionUsuario.php
+++ trunk/trueque_10/application/views/includes/sesionUsuario.php
@@ -1,4 +1,6 @@
-<?php
-	echo "<h3>Hola     ".$usuarioActual['nombre'];
-	echo anchor('usuarios/logout','cerrar sesion')." </h3>";
-?>
+<div id="estiloSesion"><?php
+	echo "<h3>Hola     ".$usuarioActual['nombre']." </h3>";
+	echo anchor('usuarios/logout','cerrar sesion');
+        ?>
+    <div style="clear: both;"></div>
+</div>
