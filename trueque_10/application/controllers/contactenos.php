1258ad4eb5c1550afd536fac599b452b876f9408
jessadrada <jessadrada@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-08 23:55:11 +0000

    esto es un ensayo asi que esta mal

---
+++ trueque_10/application/controllers/contactenos.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+//
+class Contactenos extends CI_Controller {
+    function _construct() {
+        parent::_construct();
+    }
+
+    public function index() {
+        $data['titulo'] = "Contactenos";
+	$data['activo'] = 4;
+       $data['contenido'] = 'estandar/inicio';
+        $data['title'] = 'Trueque Inicio';
+        $data['sidebar'] = 'sidebarCategorias';
+        $usuarioActual = $this->session->all_userdata();
+        if (isset($usuarioActual['nombre'])) {
+            $data['sesion'] = 'sesionUsuario';
+            $data['menu'] = 'menuUsuario';
+            $data['usuarioActual'] = $usuarioActual;
+            if ($usuarioActual['usuario_nivel'] == 0) {
+                $data['menu'] = 'menuAdministrador';
+            }
+        } else {
+            $data['sesion'] = 'sesionLogin';
+            $data['menu'] = 'menuEstandar';
+        }
+         $this->load->view('plantilla', $data);
+    }
+}
+?>
