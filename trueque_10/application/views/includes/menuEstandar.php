8c95cc6b87c7907b922cc8530633f86c90c97f75
jessadrada <jessadrada@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-08 23:57:20 +0000

    se corrigio el menu para que quede seleccionado

--- trueque_10/application/views/includes/menuEstandar.php
+++ trueque_10/application/views/includes/menuEstandar.php
@@ -1,5 +1,12 @@
 <ul>
-                    <li><?php echo anchor('productos', 'Inicio') ?></li>
+                   <!----- <li><?php echo anchor('productos', 'Inicio') ?></li>
                     <li><a href="#">Como Funciona</a></li>
-                    <li><a href="#">Cantactenos</a></li>
+                    <li><a href="#">Cantactenos</a></li>---->
+                    
+                    
+                <li <?php if(isset($activo) && $activo == 1) echo "class='activo'"; ?>><a href="<?php echo base_url(); ?>productos">Inicio</a></li>
+		
+                <li <?php if(isset($activo) && $activo == 3) echo "class='activo'"; ?>><a href="<?php echo base_url(); ?>comoFunciona">Como Funciona</a></li>
+		<li <?php if(isset($activo) && $activo == 4) echo "class='activo'"; ?>><a href="<?php echo base_url(); ?>contactenos">Contactenos</a></li>
+		
 </ul>
