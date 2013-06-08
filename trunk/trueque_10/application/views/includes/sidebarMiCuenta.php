7384112131e99fe798667bb994ff522919c97c39
jessadrada <jessadrada@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-08 06:10:29 +0000

    quitanto miga de pan

--- trunk/trueque_10/application/views/includes/sidebarMiCuenta.php
+++ trunk/trueque_10/application/views/includes/sidebarMiCuenta.php
@@ -1,8 +1,8 @@
 <ul>
-				<li><a href="#" class="more"> Inicio</a><br/><br/>
+				
 			<li>
 				<h2>Informacion</h2>
-				<p>Aqui encontraras informacion sobre tu cuenta, productos y trueques</p>
+				<p>Aqui encontraras informacion sobre tu cuenta, productos y trueques, ademas que podras administrar esta informacion segun tus necesidades.</p>
 			</li>
 			<li>
 				<h2>Mi Cuenta</h2>
