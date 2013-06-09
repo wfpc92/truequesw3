be17488c1052cc13b209abfc093b6ad566c12eb4
jennifererazo0124 <jennifererazo0124@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-09 09:32:22 +0000

    Algunos cambios para facilitar el manejo de las migas de pan

--- trunk/trueque_10/application/views/includes/sidebarCategorias.php
+++ trunk/trueque_10/application/views/includes/sidebarCategorias.php
@@ -6,23 +6,22 @@
 			<li>
 				<h2>Categorias</h2>
 				<ul>
-					<li><a href="#">Resutados De La Busqueda</a></li>
 					<li><?php echo anchor('productos','Todas las Categorias')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/antiguedades','Antiguedades')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/camaras','Camaras')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/casas','Casas')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/celulares','Celulares')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/cine','Cine')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/computadores','Computadores')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/deportes','Deporte')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/electrodomesticos','Electrodomesticos')?></li>
-					<li><?php echo anchor('productos/getProductosSide/joyas','Joyas')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/juguetes','Juguetes')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/libros','Libros')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/musica','Musica')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/vehiculos','Vehiculos')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/videojuegos','Videojuegos')?></li>
-                                        <li><?php echo anchor('productos/getProductosSide/otros','Otros')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Antiguedades','Antiguedades')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Camaras','Camaras')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Casas','Casas')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Celulares','Celulares')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Cine','Cine')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Computadores','Computadores')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Deporte','Deporte')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Electrodomesticos','Electrodomesticos')?></li>
+					<li><?php echo anchor('productos/getProductosSide/Joyas','Joyas')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Juguetes','Juguetes')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Libros','Libros')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Musica','Musica')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Vehiculos','Vehiculos')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Videojuegos','Videojuegos')?></li>
+                                        <li><?php echo anchor('productos/getProductosSide/Otros','Otros')?></li>
 				</ul>
 			</li>
 		</ul>
