56dfc21d72d38e23ecbfd08ca3c2b20657d41af0
jennifererazo0124 <jennifererazo0124@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-09 09:38:15 +0000

    Envio de email con migas

--- trunk/trueque_10/application/views/estandar/email.php
+++ trunk/trueque_10/application/views/estandar/email.php
@@ -2,29 +2,48 @@
 <?php 
 	echo form_open('usuarios/enviarContrasena');
 ?>
+<div id ="miga">
+    <?php echo anchor('productos/index','Inicio'); ?>
+     > <strong>Olvido su contraseña?</strong>
+</div>
+</br></br>
+
+
 <h1> Recuperacion de contraseña</h1>
-<table id="recuperar contrasena">
-    <tr>    <td><?php if(isset($errores)){
-        echo $errores;
-    }
-?> </td>
-		<td><br/>* E-mail: </td>
-		<td> <?php
-			$data_form = array(
-                          'name'        => 'email',
-                          'id'          => 'email',
-                          'size'        => '40'
-                        );
-			echo form_input($data_form);?>
-		</td>
-	</tr>
+<table id="recuperarContrasena">
+    <tr>
+        <td>
+            (*)Dato Obligatorio
+        </td>
+    </tr>
+    <tr> 
+        <tr>
+            <td><?php if(isset($errores)){
+                 echo $errores;
+                }
+                ?> 
+            </td>
+        </tr>
+	<td>* E-mail: </td>
+	<td> 
+            <?php
+		$data_form = array(
+                    'name'        => 'email',
+                    'id'          => 'email',
+                    'size'        => '40'
+                 );
+                echo form_input($data_form);?>
+	</td>
+    </tr>
 </table>
+<table id = "btnRegistro">
 	 <tr>
-        <td><br/><input id = "registrarse" type="submit" value="Enviar"/></td>
-        <button id = "cancelar" onclick="location.href='<?php echo base_url(); ?>'; return false;"> Cancelar</button>
-        <!--<input type= "button" name= "submitCancel"  value="Cancel" onclick="redirect('usuarios');”>-->
+            <td><input  type="submit" value="Enviar"/></td>
+            <td><button id = "cancelar" onclick="location.href='<?php echo base_url(); ?>'; return false;"> Cancelar</button></td>
+             <!--<input type= "button" name= "submitCancel"  value="Cancel" onclick="redirect('usuarios');”>-->
         <?php echo form_close();?>
     </tr>
 <?php echo form_close();?>
+</table>
 
 
