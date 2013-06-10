31a428a9cd5c958fd3d16e42ab937deda64bf06f
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-10 19:41:43 +0000

    se arreglo la visualizacion de los productos de misProductos.php

--- trueque_10/application/views/usuario/misProductos.php
+++ trueque_10/application/views/usuario/misProductos.php
@@ -14,18 +14,18 @@
             foreach ($productos->result() as $producto):
                 ?>
                 <li>
-                    <ul style="display: inline-block;">
-                        <li style="width: 50%; float: left;">
-                            <?php
+                    <ul>
+                        <li class="li_imagen">
+                           <center> <?php
                             $image_properties = array(
                                 'src' => $producto->imagen,
                                 'alt' => $producto->p_nombre,
                                 'class' => 'resize',
                             );
                             echo img($image_properties);
-                            ?>
+                            ?></center>
                         </li>
-                        <li style="width: 30%; float: left; padding-right: 20%;">
+                        <li class="li_contenido">
                             <br/>
                             <h3><?php echo anchor('productos/verProducto/' . $producto->producto_id, $producto->p_nombre); ?>
                             </h3>
@@ -33,14 +33,15 @@
                             <b>Categoria: </b><?php echo $producto->categoria; ?><br/>
                             <b>Fecha Publicaci&oacute;n: </b><?php echo $producto->fechaingreso; ?><br/>
                         </li>
-                <li style="width: 20%; float: right; padding-right: 5%;">
+                        <br/>
+                <li style="width: auto; float: left; padding-top: 4%; padding-left: 5%;">
                     <button><?php echo anchor('miCuenta/editarProducto/'.$producto->producto_id, 'Editar') ?></button>
                 </li>
-                <li style="width: 20%; float: right; padding-right: 5%;">
-                    <button><?php echo anchor('miCuenta/borrarProducto/'.$producto->producto_id, 'Eliminar') ?></button>
-                </li>
-                <li style="width: 20%; float: right; padding-right: 5%;">
+                <li style="width: auto; float: left; padding-top: 4%;padding-left: 5%;">
                     <button><?php echo anchor('miCuenta/darDeBaja/'.$producto->producto_id, 'Dejar de Publicar') ?></button>
+                </li>
+                <li style="width: auto; float: left; padding-top: 4%;padding-left: 5%;">
+                    <button><?php echo anchor('miCuenta/borrarProducto/'.$producto->producto_id, 'Eliminar') ?></button>
                 </li>
             </ul>
         </li>
