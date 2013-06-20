f75aff24e38c5a938592d344d659e395a2e5f8a3
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-20 22:23:41 +0000

    parte del correo superado con exito, se envia desde un servidor de gmail desde la cuenta truequesoporte@gmail.com, la contrasena es 123truequesoporte123 el servidor es: ssl://smtp.googlemail.com el puerto es: 465

--- trueque_10/application/config/email.php
+++ trueque_10/application/config/email.php
@@ -1,22 +1,45 @@
 <?php
-
-
 $config['useragent'] = 'CodeIgniter';//	None	The "user agent".
 $config['protocol'] = 'smtp';//	mail, sendmail, or smtp	The mail sending protocol.
-$config['mailpath'] = 'C:/xampp/htdocs/trueque_10/base_datos';//	None	The server path to Sendmail.
-$config['smtp_host'] = '192.168.43.201';//aqui va la direccion IP del computador
-$config['smtp_user'] = 'Admin';//
-$config['smtp_pass'] = '';//
-$config['smtp_port'] = '25';//	None	SMTP Port.
-$config['smtp_timeout'] = 5;//	None	SMTP Timeout (in seconds).
+//$config['mailpath'] = 'C:/xampp/htdocs/trueque_10/base_datos';//	None	The server path to Sendmail.
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';//aqui va la direccion IP del computador
+$config['smtp_user'] = 'truequesoporte@gmail.com';//
+$config['smtp_pass'] = '123truequesoporte123';//
+$config['smtp_port'] = '465';//	None	SMTP Port.
+$config['smtp_timeout'] = 30;//	None	SMTP Timeout (in seconds).
 $config['wordwrap'] = TRUE;//	TRUE or FALSE (boolean)	Enable word-wrap.
 $config['wrapchars'] = 	76;//		Character count to wrap at.
-$config['mailtype'] = 'text';//	text or html	Type of mail. If you send HTML email you must send it as a complete web page. Make sure you don't have any relative links or relative image paths otherwise they will not work.
+$config['mailtype'] = 'html';//	text or html	Type of mail. If you send HTML email you must send it as a complete web page. Make sure you don't have any relative links or relative image paths otherwise they will not work.
 $config['charset'] = 'utf-8';//		Character set (utf-8, iso-8859-1, etc.).
 $config['validate'] = TRUE;//	TRUE or FALSE (boolean)	Whether to validate the email address.
 $config['priority'] = 3;//	1, 2, 3, 4, 5	Email Priority. 1 = highest. 5 = lowest. 3 = normal.
-$config['crlf'] = '\n';//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
-$config['newline'] = '\n';//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
+$config['crlf'] = "\r\n";//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
+$config['newline'] = "\r\n";//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
 $config['bcc_batch_mode'] = FALSE;//	TRUE or FALSE (boolean)	Enable BCC Batch Mode.
 $config['bcc_batch_size'] = 200;//	None	Number of emails in each BCC batch.
-?>
+
+
+
+
+
+
+/*
+$config['useragent'] = 'CodeIgniter';//	None	The "user agent".
+$config['protocol'] = 'smtp';//	mail, sendmail, or smtp	The mail sending protocol.
+//$config['mailpath'] = 'C:/xampp/htdocs/trueque_10/base_datos';//	None	The server path to Sendmail.
+$config['smtp_host'] = 'ssl://smtp.googlemail.com';//aqui va la direccion IP del computador
+$config['smtp_user'] = 'truequesoporte@gmail.com';//
+$config['smtp_pass'] = '123truequesoporte123';//
+$config['smtp_port'] = '465';//	None	SMTP Port.
+$config['smtp_timeout'] = 30;//	None	SMTP Timeout (in seconds).
+$config['wordwrap'] = TRUE;//	TRUE or FALSE (boolean)	Enable word-wrap.
+$config['wrapchars'] = 	76;//		Character count to wrap at.
+$config['mailtype'] = 'html';//	text or html	Type of mail. If you send HTML email you must send it as a complete web page. Make sure you don't have any relative links or relative image paths otherwise they will not work.
+$config['charset'] = 'utf-8';//		Character set (utf-8, iso-8859-1, etc.).
+$config['validate'] = TRUE;//	TRUE or FALSE (boolean)	Whether to validate the email address.
+$config['priority'] = 3;//	1, 2, 3, 4, 5	Email Priority. 1 = highest. 5 = lowest. 3 = normal.
+$config['crlf'] = "\r\n";//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
+$config['newline'] = "\r\n";//	"\r\n" or "\n" or "\r"	Newline character. (Use "\r\n" to comply with RFC 822).
+$config['bcc_batch_mode'] = FALSE;//	TRUE or FALSE (boolean)	Enable BCC Batch Mode.
+$config['bcc_batch_size'] = 200;//	None	Number of emails in each BCC batch.
+*/?>
