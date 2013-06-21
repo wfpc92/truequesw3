aa248c0bd5d06522cc9018dd9af92c14ae010404
yohannazg31 <yohannazg31@3b377d38-05df-6e0d-1104-16c6b27463a3>
2013-06-21 00:27:46 +0000

    SE COLOCARON LOS LINKS PARA EL AVATAR PARA PODER EDITAR INFORMACION DE USUARIO

--- trunk/trueque_10/application/views/includes/sesionUsuario.php
+++ trunk/trueque_10/application/views/includes/sesionUsuario.php
@@ -1,21 +1,24 @@
 <div id="estiloSesion">
     <div style="float:left">
-    <div>
-        <?php echo "<h3>Hola     " . $usuarioActual['nombre'] . " </h3>";?>
-    </div>
-    <div>
-        <?php echo anchor('usuarios/logout', 'cerrar sesion');?>
-    </div>
+        <div>
+            <?php echo '<h3>Hola ' . $usuarioActual['nombre'].'</h3>'; ?>
+        </div>
+        <div>
+            <?php echo anchor('usuarios/logout', 'cerrar sesion'); ?>
+        </div>
     </div>
     <div style="float:right">
-<?php $image_properties = array(
-    'src' => $usuarioActual['avatar'],
-    'alt' => $usuarioActual['nombre'],
-    'class' => 'resize',
-    'width' => '50',
-    'height' => '50',
-);
-echo img($image_properties);?>
+        <?php
+        $image_properties = array(
+            'src' => $usuarioActual['avatar'],
+            'alt' => $usuarioActual['nombre'],
+            'class' => 'resize',
+            'width' => '50',
+            'height' => '50',
+        );
+
+        echo anchor('http://localhost/trueque_10/miCuenta/editarInformacion', img($image_properties));
+        ?>
     </div>
     <div style="clear: both;"></div>
 </div>
